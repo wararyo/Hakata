64398e87f70a79a5a1fbbe24e5912a6417f1b1f6
wararyo <wararyo@gmail.com>
2016-08-03 18:31:53 +0900

    Rename LevelLoader

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LoadLevel : MonoBehaviour {
+public class LevelLoader : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
